eb17e7ce791af0d5052bb7a97afb54d121730b61
Wu JunYi <wu65830600@163.com>
2022-05-16 00:40:58 +0800

    smooth stop server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/wujunyi792/gin-template-new/config"
 	_ "github.com/wujunyi792/gin-template-new/internal/controller/example"
@@ -9,6 +11,11 @@ import (
 	"github.com/wujunyi792/gin-template-new/internal/middleware"
 	"github.com/wujunyi792/gin-template-new/internal/redis"
 	v1 "github.com/wujunyi792/gin-template-new/internal/router/v1"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var E *gin.Engine
@@ -23,7 +30,28 @@ func init() {
 func Run() {
 	redis.GetRedis()
 	v1.MainRouter(E)
-	if err := E.Run("0.0.0.0:8080"); err != nil {
-		logger.Error.Fatalln(err)
+	srv := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: E,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error.Println("Got Server Err: ", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Info.Println("Start shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error.Fatalln("Server forced to shutdown:", err)
+	}
+
+	logger.Info.Println("Server exiting Correctly")
 }
